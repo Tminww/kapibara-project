8cf0731ed1d9b18f89ddd9b297b1aa7f5668793d
Tminww <you@example.com>
2024-09-23 18:23:45 +0300

    try: Начал пробовать делать апи

--- backend/src/api/routers.py
+++ backend/src/api/routers.py
@@ -3,6 +3,7 @@ from src.api.statistics import router as router_statistics
 from src.api.regions import router as router_regions
 from src.api.districts import router as router_districts
 from src.api.deadlines import router as router_deadlines
+from src.api.dashboard import router as router_dashboard
 
 all_routers = [
     router_subjects,
@@ -10,4 +11,5 @@ all_routers = [
     router_regions,
     router_districts,
     router_deadlines,
+    router_dashboard,
 ]
