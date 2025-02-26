f1f622a5300b3c3dc59837fd81183af7d1ddfd73
Олейник Серафим <serafim.oleynik.5777@yandex.ru>
2025-02-26 12:48:16 +0300

    fix: Исправил ошибки на бэкенде

--- backend/src/api/dependencies.py
+++ backend/src/api/dependencies.py
@@ -1,7 +1,6 @@
 from repositories.statistics import SQLAlchemyRepository
 
 from services.statistics import StatisticsService
-from services.subjects import SubjectsService
 
 
 def statistics_service():
